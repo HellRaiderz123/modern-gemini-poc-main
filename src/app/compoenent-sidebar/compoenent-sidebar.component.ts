497999014957303b5f4390589484920f076ae66f
hellraider <tarunkumarparashar@hotmail.com>
2022-08-11 17:07:01 +0100

    second

--- src/app/compoenent-sidebar/compoenent-sidebar.component.ts
+++ src/app/compoenent-sidebar/compoenent-sidebar.component.ts
@@ -12,4 +12,8 @@ export class CompoenentSidebarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  sidepopup() {
+
+  }
+
 }
--- src/app/component-main/component-main.component.ts
+++ src/app/component-main/component-main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-component-main',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./component-main.component.css']
 })
 export class ComponentMainComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
